65488b03cebcb3070425634b981152653a2030cd
Meghali Chaudhari <meghalichaudhari456@gmail.com>
2022-08-11 14:35:39 +0530

    completed

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class FurnitureItem {
+public class FurnitureItem {
+    int furnitureCode;
+    String furnitureType;
+    int gradeOfFurniture;
+    String color;
+    String furnitureUsage;
+    double price;
+    FurnitureItem(){
+        furnitureCode = 2546;
+        furnitureType ="";
+        gradeOfFurniture =0;
+        color ="";
+        furnitureUsage = "";
+        price = 250.50;
+    }
+
+    public double discount(){
+        return price - (price * 5/100);
+    }
 }
--- src/FurnitureItemImpl.java
+++ src/FurnitureItemImpl.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class FurnitureItemImpl {
+import java.util.Scanner;
+
+public class FurnitureItemImpl {
+    public static void main(String[] args) {
+        FurnitureItem obj = new FurnitureItem();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which type of Furniture usage you want ");
+        obj.furnitureUsage = scanner.nextLine();
+        System.out.println("Which type of furniture you want ");
+        obj.furnitureType = scanner.nextLine();
+        System.out.println("Which color you want in furniture");
+        obj.color = scanner.nextLine();
+        System.out.println("Enter grade");
+        obj.gradeOfFurniture = scanner.nextInt();
+
+        if(obj.furnitureUsage.equals("outdoor")) {
+            double pri = obj.discount();
+            System.out.println("Discount = " + pri);
+        }
+        else{
+            System.out.println("NO DISCOUNT");
+        }
+    }
 }
